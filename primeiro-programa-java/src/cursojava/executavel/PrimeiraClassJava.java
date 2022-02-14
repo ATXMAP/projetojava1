36bfc82887847c3f065da8a759e5bab5e0361430
ATXMAP <map201240@gmail.com>
2022-02-14 07:54:45 -0300

    Criaçâo do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClassJava {
+
+	/*Main é metodo execitavel em java*/
+	public static void main(String[] args) {
+      
+		/*Escreve texto no console*/
+		System.out.println("Meu programa em Java ");
+		System.out.println("Java é orientado a objetos");
+	}
+}
